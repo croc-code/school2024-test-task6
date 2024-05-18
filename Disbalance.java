bbd99f15ee8e85e5e30900754bef9683856cff69
tesinitsyn <sitiev174@gmail.com>
2024-05-18 19:40:33 +0300

    added class Disbalance

---
+++ Disbalance.java
@@ -0,0 +1,18 @@
+public class Disbalance {
+
+    private String fullName;
+    private double disbalance;
+
+    public Disbalance(String fullName, double disbalance) {
+        this.fullName = fullName;
+        this.disbalance = disbalance;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public double getDisbalance() {
+        return disbalance;
+    }
+}
