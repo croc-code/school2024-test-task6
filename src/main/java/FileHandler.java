8bb827c36523d9dad2887559fc7eeefd42a68030
Narko <nekit.2280.3390@gmail.com>
2024-05-22 15:53:14 +0300

    Added maven to the project. Added Main and FileHandler class

---
+++ src/main/java/FileHandler.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Класс FileHandler отвечает за чтение и запись строк из файлов.
+ */
+public class FileHandler {
+
+    /**
+     * Читает строки из файла "report.txt" и возвращает их в виде списка строк.
+     *
+     * @return Список строк, содержащий прочитанные строки из файла.
+     * @throws IOException Если файл "report.txt" не найден или возникла ошибка при чтении файла.
+     */
+    public List<String> readLines() throws IOException {
+        List<String> lines;
+        try (InputStream inputStream = getClass().getResourceAsStream("/report.txt")) {
+            if (inputStream == null) {
+                throw new IOException("File not found: report.txt");
+            }
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                lines = reader.lines().collect(Collectors.toList());
+            }
+        }
+        return lines;
+    }
+
+    /**
+     * Записывает строки в файл "result.txt".
+     *
+     * @param lines Список строк для записи в файл.
+     * @throws IOException Если произошла ошибка при записи в файл.
+     */
+    public void writeLines(List<String> lines) throws IOException {
+        String outputFilePath = "result.txt";
+        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get(outputFilePath))) {
+            for (String line : lines) {
+                bw.write(line);
+            }
+        }
+    }
+}
